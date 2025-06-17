7e2b0fe7fdc1e965b3fe2046f550b568bf709d53
Ben Cordes <cordes.ben@gmail.com>
2025-06-17 19:17:14 -0400

    gradle build

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("java")
+}
+
+group = "com.hitchhikerprod.league"
+version = "0.9-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
