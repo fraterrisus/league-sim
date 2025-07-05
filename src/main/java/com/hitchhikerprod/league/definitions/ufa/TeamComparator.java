830262a4c9ccca8e9f0de103bf5b0fbb0188bfd0
Ben Cordes <cordes.ben@gmail.com>
2025-07-04 20:36:24 -0400

    update UFA tie breakers

--- src/main/java/com/hitchhikerprod/league/definitions/ufa/TeamComparator.java
+++ src/main/java/com/hitchhikerprod/league/definitions/ufa/TeamComparator.java
@@ -13,24 +13,35 @@ public class TeamComparator implements Comparator<TeamData> {
     @Override
     public int compare(TeamData t1, TeamData t2) {
         // #1: overall win percentage
-        final int c = Double.compare(t1.getWinPercentage(), t2.getWinPercentage());
+        int c = Double.compare(t1.getWinPercentage(), t2.getWinPercentage());
         if (c != 0) return c;
 
-        // #2: head-to-head wins
         int t1HeadWins = 0;
+        int t1HeadGD = 0;
         int t2HeadWins = 0;
+        int t2HeadGD = 0;
         for (MatchDay md : matchDays) {
             for (GameData g : md.games) {
                 if (g.getAwayScore() == null || g.getHomeScore() == null) continue;
                 if (t1 == g.getAwayTeam() && t2 == g.getHomeTeam()) {
+                    t1HeadGD += g.getAwayScore() - g.getHomeScore();
+                    t2HeadGD += g.getHomeScore() - g.getAwayScore();
                     if (g.getAwayScore() > g.getHomeScore()) t1HeadWins++;
                     else t2HeadWins++;
                 } else if (t1 == g.getHomeTeam() && t2 == g.getAwayTeam()) {
+                    t1HeadGD += g.getHomeScore() - g.getAwayScore();
+                    t2HeadGD += g.getAwayScore() - g.getHomeScore();
                     if (g.getHomeScore() > g.getAwayScore()) t1HeadWins++;
                     else t2HeadWins++;
                 }
             }
         }
-        return Integer.compare(t1HeadWins, t2HeadWins);
+
+        // #2: head-to-head wins
+        c = Integer.compare(t1HeadWins, t2HeadWins);
+        if (c != 0) return c;
+
+        // #3: head-to-head goal difference
+        return Integer.compare(t1HeadGD, t2HeadGD);
     }
 }
